ca5bf77f89f5f6769127531856ea982ccf0963eb
Maria Kuzminyh <m.polynskaya@yandex.ru>
2019-09-16 15:02:16 +0500

    Доработала

---
+++ js/js.js
@@ -0,0 +1,16 @@
+/* mainNavigationMenu = document.querySelector('.mainNavigationMenu');
+window.addEventListener('scroll', function(event) {
+    let y = this.pageYOffset;
+    if (y >= 98) {
+        mainNavigationMenu.classList.add("yes");
+    } else {
+        mainNavigationMenu.classList.remove("yes");
+    }
+});
+
+
+
+let mainNavigationMenu = document.querySelector('.mainNavigationMenu');
+if (pageYOffset === 98) {
+    mainNavigationMenu.classList.add("yes");
+} */
